dabc2eae87b3f400607348d6be0868c32e77b7d4
nvidda-hub <nkvidda@gmail.com>
2022-02-02 17:39:54 +0530

    models added

--- sellerSide/admin.py
+++ sellerSide/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-from .models import Account
+from sellerSide.models import Account, Product, Customer, Order, Store
 # Register your models here.
 
 
-admin.site.register(Account)
+@admin.register(Account)
+class AccountModelAdmin(admin.ModelAdmin):
+    list_display = ["userid", "user", "mobile", "otp"]
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ["productid", "product_name", "product_desc", "product_mrp", "product_salePrice", "product_image", "product_cat"]
+
+
+@admin.register(Store)
+class StoreModelAdmin(admin.ModelAdmin):
+    list_display = ["storeid", "userid", "store_name", "store_city", "store_state", "store_pincode"]
+
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ["customerid", "customer_mobile", "customer_city", "customer_state", "customer_pincode"]
+
+
+@admin.register(Order)
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = ["orderid", "productid", "customer_mobile"]
--- sellerSide/models.py
+++ sellerSide/models.py
@@ -1,9 +1,78 @@
 from django.db import models
 from django.contrib.auth.models import User
+import random
+
+
 
 # Create your models here.
 
+random_string = "!@#$%^&(){}<>?'+_"
+
 class Account(models.Model):
+    userid = models.AutoField(primary_key=True, default=1)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     mobile = models.CharField(max_length=30)
     otp = models.CharField(max_length=6)
+
+    @property
+    def issued_token_to_user(self):
+        return f"USR_token_{random.shuffle(random_string)}&&{self.mobile[:3]}"
+
+    def __str__(self):
+        return f" {self.user}, user is created"
+
+
+class Store(models.Model):
+    storeid = models.AutoField(primary_key=True)
+    userid = models.ForeignKey(Account, on_delete=models.CASCADE)
+    store_name = models.CharField(max_length=20)
+    store_city = models.CharField(max_length=30)
+    store_state = models.CharField(max_length=30)
+    store_pincode = models.CharField(max_length=30)
+    
+    @property
+    def store_link(self):
+        return f"{self.store_name}_&_{random.shuffle(random_string)}"
+    
+    def __str__(self):
+        return f"Store created with name {self.store_name}"
+
+
+
+class Product(models.Model):
+    productid = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=30)
+    product_desc = models.TextField()
+    product_mrp = models.FloatField()
+    product_salePrice = models.FloatField()
+    product_image = models.ImageField(upload_to="product_images")
+    product_cat = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return f"product {self.product_name}"
+
+
+# class Categories(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     product_name = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Customer(models.Model):
+    customerid = models.AutoField(primary_key=True)
+    customer_mobile = models.CharField(max_length=13, unique=True)
+    customer_name = models.CharField(max_length=50)
+    customer_city = models.CharField(max_length=30)
+    customer_state = models.CharField(max_length=30)
+    customer_pincode = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"customer with name {self.customer_name}"
+
+
+class Order(models.Model):
+    orderid = models.AutoField(primary_key=True)
+    productid = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer_mobile = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"order details for customer mobile {self.customer_mobile} with {self.product_name}"
